c0843f5c504897561a3618b82b3b74e122e30413
Freshield <zxdsw199182@gmail.com>
2015-12-22 23:14:00 -0500

    nixie show

---
+++ AVR 100 EXAMPLE/TEST5_NIXIE_SHOW/TEST5_NIXIE_SHOW/TEST5_NIXIE_SHOW.cpp
@@ -0,0 +1,21 @@
+﻿/*
+ * TEST5_NIXIE_SHOW.cpp
+ *
+ * Created: 2014/2/21 18:00:43
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+ 
+int main(void)
+{
+	NIXIE_INIT();
+	
+    while(1)
+    {
+        NIXIE_FLOW();  
+    }
+}
