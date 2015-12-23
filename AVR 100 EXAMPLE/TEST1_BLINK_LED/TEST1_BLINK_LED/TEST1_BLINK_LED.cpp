cdd8526aa052fe67d5057999d664d9928315f652
Freshield <zxdsw199182@gmail.com>
2015-12-22 23:11:41 -0500

    blink led

---
+++ AVR 100 EXAMPLE/TEST1_BLINK_LED/TEST1_BLINK_LED/TEST1_BLINK_LED.cpp
@@ -0,0 +1,22 @@
+﻿/*
+ * TEST1_BLINK_LED.cpp
+ *
+ * Created: 2014/2/20 15:21:17
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_led.h>
+
+
+int main(void)
+{
+	
+	
+    while(1)
+    {
+        LED_BLINK(3,3);
+    }
+}
