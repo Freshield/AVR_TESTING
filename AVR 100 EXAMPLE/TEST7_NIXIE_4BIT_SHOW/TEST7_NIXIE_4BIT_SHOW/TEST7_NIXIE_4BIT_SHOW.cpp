1ba6b9b48596bfdf99423df73a71167f32991797
Freshield <zxdsw199182@gmail.com>
2015-12-23 23:25:06 -0500

    nixie 4bit show

---
+++ AVR 100 EXAMPLE/TEST7_NIXIE_4BIT_SHOW/TEST7_NIXIE_4BIT_SHOW/TEST7_NIXIE_4BIT_SHOW.cpp
@@ -0,0 +1,21 @@
+﻿/*
+ * TEST7_NIXIE_4BIT_SHOW.cpp
+ *
+ * Created: 2014/2/21 22:00:21
+ *  Author: Narcissus
+ */ 
+
+ 
+#include <avr/io.h> 
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+ 
+int main(void)
+{    
+	NIXIE_4BIT_INIT();     
+	  
+    while(1)       
+    {    
+       NIXIE_4BIT_SHOW(16,5,2,0); 
+    }   
+}    
