e4c65f805b504362719f882ede3b9e0118216950
Freshield <zxdsw199182@gmail.com>
2015-12-23 23:25:38 -0500

    keyboard led move

---
+++ AVR 100 EXAMPLE/TEST8_KEYBOARD_LED_MOVE/TEST8_KEYBOARD_LED_MOVE/TEST8_KEYBOARD_LED_MOVE.cpp
@@ -0,0 +1,25 @@
+﻿/*
+ * TEST8_KEYBOARD_LED_MOVE.cpp
+ *
+ * Created: 2014/2/22 16:36:42
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_led.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_keyboard.h>
+
+int main(void)
+{
+	KEYBOARD_INIT_M16();
+	
+	
+	
+	 
+    while(1)
+    {
+        KEYBOARD_MOVE_LED();
+    }
+}  
