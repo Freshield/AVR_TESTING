ea4a09e78a7b6ff86b7d295a8e52d5c6c4685def
Freshield <zxdsw199182@gmail.com>
2015-12-25 20:31:28 -0500

    alarm key

---
+++ AVR 100 EXAMPLE/TEST12_ALARM_KEY/TEST12_ALARM_KEY/TEST12_ALARM_KEY.cpp
@@ -0,0 +1,39 @@
+﻿/*
+ * TEST12_ALARM_KEY.cpp
+ *
+ * Created: 2014/3/2 20:37:18
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_keyboard.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_alarm.h>
+
+int main(void)
+{
+	ALARM_INIT();
+	
+	KEYBOARD_INIT_M16();
+	
+	//DDRA = 0XFF;
+		
+    while(1) 
+    { 
+        
+         if (KEY_DOWN() == 1) 
+        {  
+		    
+			//PORTA = 0xff;
+			ALARM(3); 
+			
+        }
+		else
+		{
+			//PORTA = 0x00;
+			CLEAR_ALARM();
+			
+		}			
+    }
+}
