010d7aebc6ae821b0c419aac0d7bf5b70f329162
Freshield <zxdsw199182@gmail.com>
2015-12-25 20:31:07 -0500

    relay bumb

---
+++ AVR 100 EXAMPLE/TEST11_RELAY_BUMB/TEST11_RELAY_BUMB/TEST11_RELAY_BUMB.cpp
@@ -0,0 +1,51 @@
+﻿/*
+ * TEST11_RELAY_BUMB.cpp
+ *
+ * Created: 2014/2/22 20:14:10
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_keyboard.h>
+
+int main(void)
+{
+	KEYBOARD_INIT_M16();
+	
+	DDRA = 0xff;
+	
+	PORTA = 0xff;
+	
+	delay_second(5);
+	
+	//PORTA = 0x00;
+	
+	uchar FUN = 0;
+	
+	
+    while(1)
+    {
+		
+		/*
+		
+        if (KEY_DOWN() == 1)
+        {
+						
+			while(KEY_DOWN());
+			
+			PORTA = ~PORTA;
+			
+			delay_reduce(20);
+				
+				
+				
+				
+			}
+			*/
+			
+			
+        
+    }
+}
