85d6313d6d39bba71688d971c1971492992cb3d5
Freshield <zxdsw199182@gmail.com>
2015-12-25 20:32:11 -0500

    int0 count

---
+++ AVR 100 EXAMPLE/TEST14_INT0_COUNT/TEST14_INT0_COUNT/TEST14_INT0_COUNT.cpp
@@ -0,0 +1,43 @@
+﻿/*
+ * TEST14_INT0_COUNT.cpp
+ *
+ * Created: 2014/3/3 16:21:50
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_external_interrupt.h>
+
+uchar COUNT_NUM = 0;
+
+ISR(INT0_vect)
+{
+	
+	COUNT_NUM ++;
+	
+	if (COUNT_NUM == 16)
+	{
+		COUNT_NUM = 0;
+	}
+	
+	
+	
+}
+
+int main(void)
+{
+	NIXIE_INIT();
+	
+	EXTERNAL_INTERRUPT0_INIT_M16();
+	
+	sei();
+	
+    while(1)
+    {
+        NIXIE_SHOW(COUNT_NUM);
+    }
+}
