624bbedaf58ed4ec4bc368cb024ea703cef99033
Freshield <zxdsw199182@gmail.com>
2015-12-28 20:31:17 -0500

    led flow test

---
+++ AVR TEST NEW/LED FLOW TEST/LED FLOW TEST/LED FLOW TEST.cpp
@@ -0,0 +1,20 @@
+﻿/*
+ * LED_FLOW_TEST.cpp
+ *
+ * Created: 2014/1/3 21:43:15
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_led.h>
+
+int main(void)
+{
+	
+    while(1) 
+    {    
+       led_flow();    
+    }
+}
