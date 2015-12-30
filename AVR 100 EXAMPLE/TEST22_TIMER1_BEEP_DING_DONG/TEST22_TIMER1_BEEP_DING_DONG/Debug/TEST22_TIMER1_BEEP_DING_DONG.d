800ceb420c2c2b2f20535f6acc440c81bb34bc48
Freshield <zxdsw199182@gmail.com>
2015-12-29 22:55:37 -0500

    timer1 beep ding dong

---
+++ AVR 100 EXAMPLE/TEST22_TIMER1_BEEP_DING_DONG/TEST22_TIMER1_BEEP_DING_DONG/Debug/TEST22_TIMER1_BEEP_DING_DONG.d
@@ -0,0 +1,50 @@
+TEST22_TIMER1_BEEP_DING_DONG.d TEST22_TIMER1_BEEP_DING_DONG.o: \
+ .././TEST22_TIMER1_BEEP_DING_DONG.cpp \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom16.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/interrupt.h \
+ E:\kuaipan\EDEN\avr\system\lib_delay.h \
+ E:\kuaipan\EDEN\avr\function\lib_pwm.h \
+ E:\kuaipan\EDEN\avr\device\lib_keyboard.h \
+ E:\kuaipan\EDEN\avr\device\lib_alarm.h
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom16.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/interrupt.h:
+
+E:\kuaipan\EDEN\avr\system\lib_delay.h:
+
+E:\kuaipan\EDEN\avr\function\lib_pwm.h:
+
+E:\kuaipan\EDEN\avr\device\lib_keyboard.h:
+
+E:\kuaipan\EDEN\avr\device\lib_alarm.h:
---
+++ AVR 100 EXAMPLE/TEST22_TIMER1_BEEP_DING_DONG/TEST22_TIMER1_BEEP_DING_DONG/TEST22_TIMER1_BEEP_DING_DONG.cpp
@@ -0,0 +1,61 @@
+﻿/*
+ * TEST22_TIMER1_BEEP_DING_DONG.cpp
+ *
+ * Created: 2014/3/7 13:24:24
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\function\lib_pwm.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_keyboard.h> 
+#include <E:\kuaipan\EDEN\avr\device\lib_alarm.h>
+
+float SOUND_HZ = 65536 - 5161;
+
+ISR(TIMER1_OVF_vect)
+{
+	RE_TURE_SOUND(); 
+	 
+	TCNT1 = SOUND_HZ;
+	
+}
+
+
+int main(void) 
+{
+	ALARM_INIT();
+	
+	KEYBOARD_INIT_M16();
+	
+	TCCR1B = 0x01;
+	
+	TIMSK = 0X00;
+	
+	TCNT1 = 65536 - 5161;
+	
+	sei();
+	
+    while(1)
+    {
+        if (KEY_DOWN() == 1)
+        {
+			TIMSK = 1 << TOIE1;
+			
+			SOUND_HZ = 65536 -5161;
+			 
+			delay_second(3); 
+			
+			SOUND_HZ = 65536 - 7373;
+			
+			delay_second(5);
+			
+			CLEAR_ALARM();
+			
+			TIMSK = 0X00;
+			
+        }
+    }
+}
