ff2737534dcfaade882c2a94547585599d8d2804
Freshield <zxdsw199182@gmail.com>
2016-01-02 20:33:08 -0500

    m16 keyboard nixie show

---
+++ AVR TEST NEW/M16 KEYBOARD NIXIE SHOW/M16 KEYBOARD NIXIE SHOW/M16 KEYBOARD NIXIE SHOW.cpp
@@ -0,0 +1,24 @@
+﻿/*
+ * M16_KEYBOARD_NIXIE_SHOW.cpp
+ *
+ * Created: 2014/1/3 23:31:25
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_keyboard.h>
+ 
+int main(void)
+{ 
+	nixie_init(); 
+	 
+    while(1) 
+    {   
+		uchar keyv = keyscan(); 
+		 
+        nixie_show(keyv);    
+    }
+}
