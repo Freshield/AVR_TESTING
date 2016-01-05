494c7113c3b8b3245775b9d4885cc6012f93e021
Freshield <zxdsw199182@gmail.com>
2016-01-04 20:34:06 -0500

    m16 nixie show

---
+++ AVR TEST NEW/M16 NIXIE SHOW/M16 NIXIE SHOW/M16 NIXIE SHOW.cpp
@@ -0,0 +1,19 @@
+﻿/*
+ * M16_NIXIE_SHOW.cpp
+ *
+ * Created: 2014/1/3 22:27:21
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+
+int main(void)
+{
+    while(1) 
+    {
+        nixie_flow(); 
+    }
+}
