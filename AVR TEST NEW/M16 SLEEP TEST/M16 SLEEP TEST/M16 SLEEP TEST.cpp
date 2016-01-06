5db9697412fe08e291a53292f29320df5adef5aa
Freshield <zxdsw199182@gmail.com>
2016-01-05 22:49:45 -0500

    sleep test

---
+++ AVR TEST NEW/M16 SLEEP TEST/M16 SLEEP TEST/M16 SLEEP TEST.cpp
@@ -0,0 +1,54 @@
+﻿/*
+ * M16_SLEEP_TEST.cpp
+ *
+ * Created: 2014/1/5 15:37:07
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_external_interrupt.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_keyboard.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_sleep.h>
+uchar keyv;
+
+ISR(INT0_vect)
+{
+   sleep_disable();
+	
+} 
+
+ 
+int main(void)
+{
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	nixie_init();
+	KEYBOARD_INIT_M16();
+	EXTERNAL_INTERRUPT0_INIT_M16();
+	
+	sei();
+	
+	
+    while(1)
+    {
+		keyv = keyscan();
+		
+       if (keyv == 16)
+       {
+      
+     
+			sleep_enable();
+			
+			sleep_cpu();
+			
+			sleep_disable();
+		 }	
+		 
+			 nixie_show(keyv);
+		          	
+    }
+}
