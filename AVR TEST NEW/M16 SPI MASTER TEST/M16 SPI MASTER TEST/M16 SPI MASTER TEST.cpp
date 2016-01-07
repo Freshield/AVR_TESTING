2f19f277d817d80048f5db3ce5c3694cdd783d0c
Freshield <zxdsw199182@gmail.com>
2016-01-06 22:08:27 -0500

    spi master test

---
+++ AVR TEST NEW/M16 SPI MASTER TEST/M16 SPI MASTER TEST/M16 SPI MASTER TEST.cpp
@@ -0,0 +1,38 @@
+﻿/*
+ * M16_SPI_MASTER_TEST.cpp
+ *
+ * Created: 2014/1/18 23:05:25
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\function\lib_spi.h>
+
+ISR(SPI_STC_vect)
+{
+	
+}
+
+
+int main(void)
+{
+	
+	SPI_MASTER_M16_init();
+	
+	sei();
+	
+	
+    while(1)
+    {
+        for (char i = 0;i < 10;i ++)
+        {
+			SPI_MasterTransmit(i);
+			
+			delay_second(30);
+			
+        }
+    }
+}
