e417f205a44345578dba42549cd18c32967146de
Freshield <zxdsw199182@gmail.com>
2016-01-08 17:36:37 -0500

    TIMER1 FAST PWM TEST

---
+++ AVR TEST NEW/M16 TIMER1 FAST PWM TEST/M16 TIMER1 FAST PWM TEST/M16 TIMER1 FAST PWM TEST.cpp
@@ -0,0 +1,48 @@
+﻿/*
+ * M16_TIMER1_FAST_PWM_TEST.cpp
+ *
+ * Created: 2014/1/13 16:22:08
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\function\lib_pwm.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_led.h>
+
+ISR(TIMER1_COMPA_vect)
+{
+	
+	
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	 
+}
+
+
+ 
+int main(void)
+{
+	
+	TIMER1_PHASE_CORRECTION_10_init();
+	
+	sei(); 
+	
+    while(1)
+    {
+      
+	 for (uchar i = 0;i <10;i ++)
+	   {
+		   delay_second(30);
+		   
+		   OCR1A = i * 128;
+		   
+		   delay_second(30);
+	   }
+	 
+    }
+}
