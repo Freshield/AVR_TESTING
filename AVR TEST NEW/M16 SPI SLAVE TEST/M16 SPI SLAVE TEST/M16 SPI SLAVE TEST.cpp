c4985dffc0a255336ca5e7f2504424bc1345af51
Freshield <zxdsw199182@gmail.com>
2016-01-10 17:35:28 -0500

    SPI SLAVE TEST

---
+++ AVR TEST NEW/M16 SPI SLAVE TEST/M16 SPI SLAVE TEST/M16 SPI SLAVE TEST.cpp
@@ -0,0 +1,40 @@
+﻿/*
+ * M16_SPI_SLAVE_TEST.cpp
+ *
+ * Created: 2014/1/18 23:15:33
+ *  Author: Narcissus
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <E:\kuaipan\EDEN\avr\system\lib_delay.h>
+#include <E:\kuaipan\EDEN\avr\function\lib_spi.h>
+#include <E:\kuaipan\EDEN\avr\device\lib_nixie.h>
+
+char get = 0;
+
+ISR(SPI_STC_vect)
+{
+	
+	get = SPDR;
+	
+	nixie_show(get);
+	
+}
+
+int main(void)
+{
+	SPI_SLAVE_M16_init();
+	
+	nixie_init();
+	
+	sei();
+	
+	nixie_show(get);
+	
+    while(1)
+    {
+        
+    }
+}
